7a96c7bd444f996e7e56505cc50a265d397573bd
edgard <edgard.habanbou@hotmail.com>
2023-11-18 18:25:58 +0200

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
+import HomePage from "./pages/home/index.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+function Home() {
+  return <HomePage />;
+}
+
+function NotFound() {
+  return <h1>404 Not Found</h1>;
+}
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
