c55b8343499a4c226f33af91b32412b6bd18bfb2
AliHakim773 <alihakim7732@gmail.com>
2023-11-21 10:00:09 +0200

    added movies carousel

---
+++ src/components/common/MovieCarousel/index.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react"
+import "./styles.css"
+import MoviesItem from "../MoviesItem"
+
+function MoviesCarousel({ title, movies }) {
+    const [screenWidth, setScreenWidth] = useState(getCurrentWidth())
+    const [itemsNumber, setItemNumber] = useState(4)
+    const [activeIndex, setActiveIndex] = useState(0)
+    const maxItems = movies.length
+
+    function getCurrentWidth() {
+        return window.innerWidth
+    }
+
+    useEffect(() => {
+        const updateWidth = () => {
+            setScreenWidth(getCurrentWidth())
+        }
+        window.addEventListener("resize", updateWidth)
+        if (screenWidth < 1000) {
+            setItemNumber(3)
+        } else if (screenWidth < 500) {
+            setItemNumber(2)
+        }
+        return () => {
+            window.removeEventListener("resize", updateWidth)
+        }
+    }, [screenWidth])
+
+    const handlePrevClick = () => {
+        let newIndex = activeIndex - 1
+        if (newIndex < 0) {
+            newIndex = maxItems / itemsNumber - 1
+        }
+        console.log(newIndex)
+        setActiveIndex(newIndex)
+    }
+    const handleNextClick = () => {
+        let newIndex = activeIndex + 1
+        if (newIndex >= maxItems / itemsNumber) {
+            newIndex = 0
+        }
+        console.log(newIndex)
+        setActiveIndex(newIndex)
+    }
+
+    return (
+        <div className='row'>
+            <div className='header'>
+                <h3 className='title'>{title}</h3>
+            </div>
+            <div className='container'>
+                <button
+                    className='handle left-handle'
+                    onClick={handlePrevClick}>
+                    <div className='text'>&#8249;</div>
+                </button>
+                <div
+                    className='slider'
+                    style={{
+                        transform: `translateX(-${activeIndex * 100}%)`,
+                    }}>
+                    {movies.map(() => {
+                        return <MoviesItem />
+                    })}
+                </div>
+                <button
+                    className='handle right-handle'
+                    onClick={handleNextClick}>
+                    <div className='text'>&#8250;</div>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default MoviesCarousel
--- src/pages/genre/index.jsx
+++ src/pages/genre/index.jsx
@@ -1,95 +1,15 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import "./styles.css"
 import StickyNav from "../../components/common/StickyNav"
 import Hero from "../../components/genre/Hero"
-import MoviesItem from "../../components/common/MoviesItem"
+import MoviesCarousel from "../../components/common/MovieCarousel"
 
 function Genre() {
-    const [screenWidth, setScreenWidth] = useState(getCurrentWidth())
-    const [itemsNumber, setItemNumber] = useState(4)
-    const [activeIndex, setActiveIndex] = useState(0)
-    const maxItems = 12
-
-    function getCurrentWidth() {
-        return window.innerWidth
-    }
-
-    useEffect(() => {
-        const updateWidth = () => {
-            setScreenWidth(getCurrentWidth())
-        }
-        window.addEventListener("resize", updateWidth)
-        if (screenWidth < 1000) {
-            setItemNumber(3)
-        } else if (screenWidth < 500) {
-            setItemNumber(2)
-        }
-        return () => {
-            window.removeEventListener("resize", updateWidth)
-        }
-    }, [screenWidth])
-
-    const handlePrevClick = () => {
-        let newIndex = activeIndex - 1
-        if (newIndex < 0) {
-            newIndex = maxItems / itemsNumber - 1
-        }
-        console.log(newIndex)
-        setActiveIndex(newIndex)
-    }
-    const handleNextClick = () => {
-        let newIndex = activeIndex + 1
-        if (newIndex >= maxItems / itemsNumber) {
-            newIndex = 0
-        }
-        console.log(newIndex)
-        setActiveIndex(newIndex)
-    }
-
     return (
         <div className='genre-page'>
             <StickyNav />
             <Hero />
-            <div className='row'>
-                <div className='header'>
-                    <h3 className='title'>Title</h3>
-                </div>
-                <div className='container'>
-                    <button
-                        className='handle left-handle'
-                        onClick={handlePrevClick}>
-                        <div className='text'>&#8249;</div>
-                    </button>
-                    <div
-                        className='slider'
-                        style={{
-                            transform: `translateX(-${activeIndex * 100}%)`,
-                        }}>
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                        <MoviesItem />
-                    </div>
-                    <button
-                        className='handle right-handle'
-                        onClick={handleNextClick}>
-                        <div className='text'>&#8250;</div>
-                    </button>
-                </div>
-            </div>
+            <MoviesCarousel movies={[1, 2, 3, 4]} />
         </div>
     )
 }
